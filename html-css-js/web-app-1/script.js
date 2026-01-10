603d8cdc4d98a6312e11cea7c7f79a76ae77193d
saithanmaideekonda718-ai <saithanmaideekonda718@gmail.com>
2026-01-10 15:04:49 +0530

    updated

--- html-css-js/web-app-1/script.js
+++ html-css-js/web-app-1/script.js
@@ -1,27 +1,32 @@
 function dispMessage(){
-    // a=10
-    // b=20
-    // c=a+b
-    //alert("Hello")
-    // alert(c)
-    // alert(document.getElementById("txtemail").value)
-    // lblMessage.innerHTML = document.getElementById("txtemail").value + "-" + document.getElementById("txtpassword").value
     let email=document.getElementById("txtemail").value
     let pass=document.getElementById("txtpassword").value
-    if(email==="john@gmail.com" && pass==="1234")
-    {
-        lblMessage.innerHTML="Welcome";
+    let found=users.find
+    (element=> element.email===email && element.password===pass);
+    if(found){
+        lblMessage.innerHTML="welcome " +found.name;
     }
     else{
-        lblMessage.innerHTML="Access denied";
+        lblMessage.innerHTML="acess denied";
     }
+}
+let users=[];
+function adduser()
+{
+    let name=document.getElementById("txtname").value
+    let email=document.getElementById("txtemail").value
+    let pass=document.getElementById("txtpassword").value
+    let user={name: name,email:email,password:pass}
+    users.push(user);
+    console.log(users);
+    showloginform();
 }
 function showloginform()
 {
     let str=`<h3>Login form</h3>
         <p><label id="lblMessage"></label></p>
-       <p><input type="text" id="txtemail"></p> 
-       <p><input type="password" id="txtpassword"></p>
+       <p><input type="text" id="txtemail" placeholder="Email"></p> 
+       <p><input type="password" id="txtpassword" placeholder="Password"></p>
        <p><button class="login-btn" onclick="dispMessage()">Login</button></p>
        <hr>
        <p><button class="register-btn" onclick="showRegisterForm()">Create account</button></p>`
@@ -31,6 +36,25 @@ function showRegisterForm()
 {
     let str=`
     <h3>Registration form</h3>
+    <p><label id="lblMessage"></label></p>
+    <p><input type="text" id="txtname" placeholder="name"></p> 
+    <p><input type="text" id="txtemail" placeholder="Email"></p> 
+    <p><input type="password" id="txtpassword" placeholder="Password"></p>
+    <p><button class="login-btn" onclick="adduser()">Submit</button></p>
+    <hr>
+      <p>Already a member?<a href="#" onclick="showloginform()">Login here</a></p>
     `
     root.innerHTML=str
+}
+function Register()
+{
+    let email=document.getElementById("txtemail").value
+    let pass=document.getElementById("txtpassword").value
+    if(email==="" || pass==="")
+    {
+        lblMessage.innerHTML="Fill all the details";
+    }
+    else{
+        lblMessage.innerHTML="Successfully registered";
+    }
 }
