b4a46c42fbe1a162632d23db0315ce429ec47350
saithanmaideekonda718-ai <saithanmaideekonda718@gmail.com>
2026-01-10 11:03:14 +0530

    updated

---
+++ html-css-js/web-app-1/script.js
@@ -0,0 +1,9 @@
+function dispMessage(){
+    // a=10
+    // b=20
+    // c=a+b
+    //alert("Hello")
+    // alert(c)
+    // alert(document.getElementById("txtemail").value)
+    lblMessage.innerHTML = document.getElementById("txtemail").value
+}
